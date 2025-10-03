b6c02498fb6dea2ce44c0376761b82484e497baf
PeruDoxer5 <perudoxer@gmail.com>
2025-10-03 12:40:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import math
 import hashlib
 import secrets
 import time
+import os
 from flask_cors import CORS, cross_origin
 
 DB_PATH = "multiplataforma.db"
@@ -1197,4 +1198,5 @@ if __name__ == "__main__":
     init_hist_db()
     init_compras_db()
     init_keys_db()
-    app.run(host="0.0.0.0", port=4764, debug=True)
+port = int(os.environ.get('PORT', '4764'))
+app.run(host='0.0.0.0', port=port, debug=False)
